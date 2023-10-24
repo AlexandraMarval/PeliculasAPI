f3d271eafff57574c33d4bb90b20ef3837ca1645
Alexandra marval <alexandraI.Marvala@gmail.com>
2023-10-24 15:51:08 +0200

    Agregue carpeta de Context agregue clase peliculaDbContext Agregue carpeta de Entitidades agregue clase CategoriaEntity

---
+++ PeliculasAPI/Context/PeliculaDbContext.cs
@@ -0,0 +1,9 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace PeliculasAPI.Context
+{
+    public class PeliculaDbContext : DbContext
+    {
+        public PeliculaDbContext(DbContextOptions options) : base(options) { }    
+    }
+}
--- PeliculasAPI/Program.cs
+++ PeliculasAPI/Program.cs
@@ -1,6 +1,9 @@
+using PeliculasAPI.Context;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
+builder.Services.AddSqlServer<PeliculaDbContext>(builder.Configuration.GetConnectionString("DefaultConnection"));
 
 builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
