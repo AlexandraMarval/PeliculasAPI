757b6fa162c2125b6bae83267a248015ca991385
Alexandra marval <alexandraI.Marvala@gmail.com>
2023-10-27 15:41:52 +0200

    agregue una nueva clase ActoresController

---
+++ PeliculasAPI/Controllers/ActoresController.cs
@@ -0,0 +1,11 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace PeliculasAPI.Controllers
+{
+    [ApiController]
+    [Route("api/actores")]
+    public class ActoresController : ControllerBase
+    {
+
+    }
+}
