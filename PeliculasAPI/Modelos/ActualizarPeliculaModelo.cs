c99f04fdfbbb94ffc78306e9f99f7567d4a87d51
Alexandra marval <alexandraI.Marvala@gmail.com>
2023-12-11 10:59:51 +0100

    Add nueva modelo ActualizarpeliculaModelo Add Propidades

---
+++ PeliculasAPI/Modelos/ActualizarPeliculaModelo.cs
@@ -0,0 +1,15 @@
+﻿using PeliculasAPI.Validaciones;
+using System.ComponentModel.DataAnnotations;
+
+namespace PeliculasAPI.Modelos
+{
+    public class ActualizarPeliculaModelo
+    {
+        [Required]
+        [StringLength(300)]
+        public string Titulo { get; set; }
+        [PesoArchivoValidacion(PesoMaximEnMegaBytes: 4)]
+        [TipoArchivoValidacion(GrupoTipoArchivo.imagen)]
+        public IFormFile Poster { get; set; }
+    }
+}
