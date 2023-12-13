a141fc53c37d0ab9232b0f5344f96cd5b8c6f93a
Alexandra marval <alexandraI.Marvala@gmail.com>
2023-12-13 10:29:28 +0100

    Add metodo de configuracion de asifnación de propiedades

--- PeliculasAPI/Context/PeliculaDbContext.cs
+++ PeliculasAPI/Context/PeliculaDbContext.cs
@@ -9,8 +9,16 @@ namespace PeliculasAPI.Context
         public DbSet<ActorEntity> Actores { get; set; }
         public DbSet<PeliculasActores> PeliculasActores { get; set; }
         public DbSet<PeliculasCategorias> PeliculasCategorias { get; set; }
-
         public DbSet<PeliculaEntidad> peliculas { get; set; }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.Entity<PeliculasActores>().HasKey(model => new {model.ActorId, model.PeliculaId});
+
+            modelBuilder.Entity<PeliculasCategorias>().HasKey(model => new { model.CategoriaId, model.PeliculaId});
+
+            base.OnModelCreating(modelBuilder);
+        }
         public PeliculaDbContext(DbContextOptions options) : base(options) { }    
     }
 }
