7fdd61ce796f6d74060f92a698f63c2729e460c3
Alexandra marval <alexandraI.Marvala@gmail.com>
2023-12-20 11:44:29 +0100

    Add CrearSalaDeCineModelo con sus respectivas propiedad

---
+++ PeliculasAPI/Modelos/CrearSalaDeCineModelo.cs
@@ -0,0 +1,11 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace PeliculasAPI.Modelos
+{
+    public class CrearSalaDeCineModelo
+    {
+        [Required]
+        [StringLength(120)]
+        public string Nombre {  get; set; }
+    }
+}
