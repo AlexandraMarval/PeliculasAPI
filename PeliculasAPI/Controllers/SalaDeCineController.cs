469258be4cd5fdde72d1b0046159872b82af365d
Alexandra marval <alexandraI.Marvala@gmail.com>
2023-12-20 12:00:37 +0100

    Add SalaDeCineController Add ISalaDeCineServicio

---
+++ PeliculasAPI/Controllers/SalaDeCineController.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace PeliculasAPI.Controllers
+{
+    [ApiController]
+    [Route("api/salasDeCine")]
+    public class SalaDeCineController : ControllerBase
+    {
+        public SalaDeCineController()
+        {
+            
+        }
+    }
+}
---
+++ PeliculasAPI/Servicios/ISalaDeCineServicio.cs
@@ -0,0 +1,9 @@
+﻿using PeliculasAPI.Modelos;
+
+namespace PeliculasAPI.Servicios
+{
+    public interface ISalaDeCineServicio
+    {
+        Task<List<SalaDeCineModelo>> ObtenerSalaDeCine();
+    }
+}
