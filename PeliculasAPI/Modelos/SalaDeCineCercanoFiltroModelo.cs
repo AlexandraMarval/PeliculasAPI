35377721fd6634cfcb921b635a4f3b75e166f195
Alexandra marval <alexandraI.Marvala@gmail.com>
2023-12-28 13:43:16 +0100

    Add un nuevo Modelo SalaDeCineCercanoModelo

--- PeliculasAPI/Modelos/SalaDeCineCercanoFiltroModelo.cs
+++ PeliculasAPI/Modelos/SalaDeCineCercanoFiltroModelo.cs
@@ -6,7 +6,13 @@ namespace PeliculasAPI.Modelos
     {
         [Range(-90, 90)]
         public double Latitud { get; set; }
-        [Range(-90, 90)]
+        [Range(-180, 180)]
         public double Longitud { get; set; }
+        private int distanciaEnKms = 10;
+        private int distanciaMaximaKms = 50;
+        public int DistanciaEnKms
+        {
+            get { return distanciaEnKms; } set { distanciaEnKms = (value > distanciaMaximaKms) ? distanciaMaximaKms : value; }
+        }
     }
 }
---
+++ PeliculasAPI/Modelos/SalaDeCineCercanoModelo.cs
@@ -0,0 +1,7 @@
+﻿namespace PeliculasAPI.Modelos
+{
+    public class SalaDeCineCercanoModelo : SalaDeCineModelo
+    {
+        public double DistanciaEnMetros {  get; set; }
+    }
+}
